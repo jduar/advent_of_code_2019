c10aa17d1b0ccdc08497a563a04e15d7d8b8cc2b
TheSupport101 <jpduarte120@gmail.com>
2019-12-02 14:24:34 +0000

    Day 1 - Part 2

---
+++ 1_tiranny_part2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+def fuel(mass):
+    total_fuel = 0
+    while mass > 0:
+        mass = (mass // 3) - 2
+        if mass > 0:
+            total_fuel += mass
+    return total_fuel
+
+def main():
+    total_fuel = 0
+    file_name = sys.argv[1]
+    with open(file_name, 'r') as file:
+        for line in file:
+            total_fuel += fuel(int(line))
+    print(total_fuel)
+
+if __name__ == "__main__":
+    main()
